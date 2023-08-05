50dc1ed07bc8aee74fab860088e0154259c79761
Naiemul Islam <39267823+Naiem1@users.noreply.github.com>
2023-08-06 03:03:42 +0600

    Update postSlice.js

--- postSlice.js
+++ postSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'; // Not installed
 
 import axios from 'axios'; // Not installed
 
